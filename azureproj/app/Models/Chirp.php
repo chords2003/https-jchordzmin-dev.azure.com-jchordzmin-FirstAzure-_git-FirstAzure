e31fd898aaf931199bc96f6645868eb45fc915c8
Jack Joseph <jackj@Jacks-MBP.attlocal.net>
2023-12-11 12:09:21 -0500

    Added the ability to view messages from the database

--- azureproj/app/Models/Chirp.php
+++ azureproj/app/Models/Chirp.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Chirp extends Model
 {
@@ -11,4 +12,10 @@ class Chirp extends Model
     protected $fillable = [
         'message',
     ];
+
+    //add chirp to user relationship
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
